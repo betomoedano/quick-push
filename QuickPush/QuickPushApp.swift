cb5ed85ae865c912accc1a01f9f84bc6de3220da
betomoedano <betomoedano@outlook.com>
2025-02-20 18:19:19 -0600

    Initial Commit

---
+++ QuickPush/QuickPushApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPushApp.swift
+//  QuickPush
+//
+//  Created by beto on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickPushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
